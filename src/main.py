0e6d8a6ef54c7b2d90d700b61c127a21ba9e856f
OleksandrBabilia <djesssash1708@gmail.com>
2025-02-20 13:39:02 +0200

    GPU float optimization

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import functional as F
+import time
 
 from configs.gptconfig import GPTConfig 
 from models.gpt import GPT
@@ -19,17 +20,23 @@ if __name__ == "__main__":
     num_return_sequences = 5
     max_length = 30
 
-    train_loader = DataLoaderLite(B=4, T=32)
-
+    train_loader = DataLoaderLite(B=2, T=1024)
+    torch.set_float32_matmul_precision("high")
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
     for i in range(50):
+        t0 = time.time()
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
-        logits, loss = model(x, y)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
         loss.backward()
         optimizer.step()
-        print(f"step {i}, loss {loss.item()}")
+        torch.cuda.synchronize()
+        t1 = time.time()
+        dt = (t1 - t0) * 1000
+        tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
+        print(f"step {i} | loss {loss.item()} | dt: {dt:.2f}ms, tok/sec: {tokens_per_second:.2f}")
     
     import sys; sys.exit(0)
 
